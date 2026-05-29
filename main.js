2d22039d8a24b984df8646473aedfc9d6d199c74
stg3barx <stg3barx@gmail.com>
2026-05-29 16:29:14 -0500

    Layout structure and initial styling

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 800,
+    width: 600,
+    height: 1000,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
